d61891266abf9fcb76d240899b1fc03d3f918733
Lempereurjulien <lempereur.julien83@gmail.com>
2025-03-22 23:33:50 +0100

    :tada: initialisation

---
+++ src/component/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+import Image from "../asset/image.jpeg"
+
+export function Navbar(){
+    return (
+        <div className="navbar-content">
+                <img className="pp" src={Image}></img>
+            <div>
+            </div>
+            <div>
+                teste
+            </div>
+            <div>
+                teste2
+            </div>
+        </div>
+    );
+}
